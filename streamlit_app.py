92596275b0ae5a8f16125260efada4e7c9caee9a
Nick Odlind <69474443+nodlind@users.noreply.github.com>
2024-09-11 21:48:08 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
+
 st.title("🎈 Read Google Sheet as DataFrame")
 
-conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(worksheet="Daily Metrics")
+conn_hm = st.connection("healthmetrics", type=GSheetsConnection)
+df_hm = conn_hm.read(worksheet="Daily Metrics")
 
-st.dataframe(df)
+st.dataframe(df_hm)
+
+conn_wo = st.connection("workouts", type=GSheetsConnection)
+df_wo = conn_wo.read(worksheet="Workouts")
+
+st.dataframe(df_wo)
